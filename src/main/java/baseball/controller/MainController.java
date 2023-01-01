c6f18cf01932bd596cbfa3bebc83aaf7290af237
JH-web115 <78305338+JH-web115@users.noreply.github.com>
2023-01-02 00:44:47 +0900

    feat: 유저의 수 입력 과정

--- src/main/java/baseball/controller/MainController.java
+++ src/main/java/baseball/controller/MainController.java
@@ -1,20 +1,32 @@
 package baseball.controller;
 
 import baseball.model.Balls;
+import baseball.model.PlayResult;
 import baseball.util.RandomGenerator;
+import baseball.view.InputView;
 import baseball.view.OutputView;
+
+import java.util.List;
 
 public class MainController {
 
+    private final InputView inputView = new InputView();
     private final OutputView outputView = new OutputView();
     private Balls computer;
+    private PlayResult result;
 
     private void init() {
         outputView.printGameStart();
         computer = new Balls(RandomGenerator.createRandomBalls());
+        result = new PlayResult();
     }
 
     public void start() {
         init();
+
+        while (result.isContinue()) {
+            List<Integer> inputBalls = inputView.readBallNumber();
+            result = computer.play(inputBalls);
+        }
     }
 }
