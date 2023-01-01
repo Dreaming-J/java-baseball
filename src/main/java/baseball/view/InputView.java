39e755d5f429a3f43dc147ae49c00b4c953b1859
JH-web115 <78305338+JH-web115@users.noreply.github.com>
2023-01-02 01:06:37 +0900

    feat: 재시작 커맨드 입력

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,5 +1,6 @@
 package baseball.view;
 
+import baseball.model.RetryOption;
 import baseball.util.TypeConverter;
 import camp.nextstep.edu.missionutils.Console;
 
@@ -8,9 +9,15 @@ import java.util.List;
 public class InputView {
 
     private static final String BALL_NUMBER_INPUT_MSG = "숫자를 입력해주세요 : ";
+    private static final String RETRY_INPUT_MSG = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public List<Integer> readBallNumber() {
         System.out.print(BALL_NUMBER_INPUT_MSG);
         return TypeConverter.toList(Console.readLine());
+    }
+
+    public RetryOption readRetryCommand() {
+        System.out.println(RETRY_INPUT_MSG);
+        return RetryOption.from(Console.readLine());
     }
 }
