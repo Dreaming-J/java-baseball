cdc8d5241146509fb8a0987c49bc81b6bfd329e7
JH-web115 <78305338+JH-web115@users.noreply.github.com>
2023-01-01 15:12:40 +0900

    feat: 볼 숫자 서로 다른 3자리 검증

--- src/main/java/baseball/model/Balls.java
+++ src/main/java/baseball/model/Balls.java
@@ -5,12 +5,21 @@ import java.util.stream.Collectors;
 
 public class Balls {
 
+    public static final String WRONG_BALLS_NUMBER_TYPE = "볼 숫자는 서로 다른 3자리 숫자여야 합니다.";
+    public static final int BALLS_SIZE = 3;
     private final List<Ball> answers;
 
     public Balls(List<Integer> answers) {
+        validate(answers);
         this.answers = answers.stream()
                 .map(answer -> new Ball(answers.indexOf(answer), answer))
                 .collect(Collectors.toList());
+    }
+
+    private void validate(List<Integer> answers) {
+        if (answers.stream().distinct().count() != BALLS_SIZE) {
+            throw new IllegalArgumentException(WRONG_BALLS_NUMBER_TYPE);
+        }
     }
 
     public PlayResult play(List<Integer> balls) {
--- src/test/java/baseball/model/ValidationBallTest.java
+++ src/test/java/baseball/model/ValidationBallTest.java
@@ -2,6 +2,8 @@ package baseball.model;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
@@ -14,6 +16,15 @@ public class ValidationBallTest {
         assertThatThrownBy(() -> new Ball(0, 0))
                 .isInstanceOf(IllegalArgumentException.class);
         assertThatThrownBy(() -> new Ball(0, 10))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 야구_숫자_중복_없는_3자리_검증() {
+        assertDoesNotThrow(() -> new Balls(List.of(1, 2, 3)));
+        assertThatThrownBy(() -> new Balls(List.of(1, 1, 2)))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Balls(List.of(1, 2, 3, 4)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
